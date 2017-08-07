b0b986898d0b3e3d5a16ca6f58969ebe4feddb45
Tyler Reitz <tdreitz@gmail.com>
2017-08-06 23:18:36 -0700

    Constructor feedback

--- ClozeCard.js
+++ ClozeCard.js
@@ -2,13 +2,19 @@
 var Cloze = function (text, cloze) {
   this.partial = partial;
   this.cloze = cloze;
+
+  // I would still keep these on the prototype
   this.printCloze = function(){
     console.log(this.cloze);
   }
+
+  // I would still keep these on the prototype  
   this.printPartial = function(){
     console.log(this.partial);
   }
+
 }
+
 Cloze.prototype.printFull = function(){
     console.log(this.partial.replace('[cloze]', this.cloze));
 }
